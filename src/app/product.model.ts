ab1b4efecb155e4246f09bef01203eb5f5100647
Eden Fils <ednfils@gmail.com>
2020-03-04 17:45:51 -0400

    Creating product model

---
+++ src/app/product.model.ts
@@ -0,0 +1,11 @@
+export class Product {
+    constructor (
+            public sku: string,
+            public name: string,
+            public imgUrl: string,
+            public department: string[],
+            public price: number ) {
+        
+
+    }
+}
